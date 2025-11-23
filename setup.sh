89a177a568902054ef034c57b445178d0bc12c1d
Nova <adityaprakash1357908@gmail.com>
2025-11-23 20:08:47 +0530

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Change INTERVAL_SECONDS value for how often you want the script to send metrics.
+#Change PROJECT_ROOT to where the project is located
+INTERVAL_SECONDS=15
+PROJECT_ROOT="/home/nova/Projects" 
+
+PY_EXEC="$PROJECT_ROOT/server-monitoring/venv/bin/python" 
+PUSH="$PROJECT_ROOT/metrics_push.py"
+
+chmod +x "$PUSH" 
+chmod +x "$PROJECT_ROOT/scripts/cpu.sh"
+chmod +x "$PROJECT_ROOT/scripts/disk.sh"
+chmod +x "$PROJECT_ROOT/scripts/memory.sh"
+
+echo "Set up done"
+while true; do
+    echo "Started"
+    $PY_EXEC $PUSH
+    sleep $INTERVAL_SECONDS
+done
